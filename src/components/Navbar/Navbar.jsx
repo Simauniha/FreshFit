fa6689c2686ccccc2f14944c00acbcfc21c21a6e
Simauniha <2005.simaunihao@gmail.com>
2026-06-17 04:33:02 +0530

    navbar is half done responsiveness is left

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import "./Navbar.css";
 import Logo from "../../assets/logo-name.png"
-import { NavLink } from 'react-router-dom';
+import { NavLink , Link } from 'react-router-dom';
+import { FaSearch, FaUser,FaShoppingCart } from "react-icons/fa";
 
 const Navbar = () => {
     return (
         <>
             <div className="flex items-center justify-between py-5 font-medium">
                 <img src={Logo} className="w-36" alt="Logo" />
-                <ul className="hidden sm:flex gap-5 gap-5 text-sm text-gray-700">
+                <ul className="hidden sm:flex gap-5 gap-5 text-lg text-gray-700">
                     <NavLink to="/" className="flex flex-col items-center gap-1">
                         <p>Home</p>
                         <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
@@ -30,8 +31,25 @@ const Navbar = () => {
                     </NavLink>
                 </ul>
 
-                <div>
-                    
+                <div className='flex items-center gap-6'>
+                    <button className='w-5 cursor-pointer'><FaSearch className="text-xl" /></button>
+
+                    <div className='group relative'>
+                        <button className='w-5 cursor-pointer'><FaUser className="text-xl" /></button>
+
+                        <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                            <div className="flex flex-col gap-2 w-36 py-5 bg-slate-100 text-gray-500 rounded shadow-lg">
+                                <p className='cursor-pointer hover:text-black'>My Profile</p>
+                                <p className='cursor-pointer hover:text-black'>Orders</p>
+                                <p className='cursor-pointer hover:text-black'>Log Out</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <Link to="/cart" className='relative'>
+                    <button className='w-5 min-w-5 cursor-pointer'><FaShoppingCart className="text-xl"/></button>
+                    <p className='absolute right-[-7px] bottom-[-2px] w-4 text-center leading-4 bg-white text-black aspect-square border border-black rounded-full text-[10px] '>10</p>
+                    </Link>
                 </div>
 
             </div>
