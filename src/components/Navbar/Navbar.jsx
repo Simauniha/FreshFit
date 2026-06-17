2ff852a8b572fbb3d42b36d5d4b9ac1b70614c8d
Simauniha <2005.simaunihao@gmail.com>
2026-06-17 14:32:29 +0530

    navbar is done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css";
 import Logo from "../../assets/logo-name.png"
-import { NavLink , Link } from 'react-router-dom';
-import { FaSearch, FaUser,FaShoppingCart } from "react-icons/fa";
+import { NavLink, Link } from 'react-router-dom';
+import { FaSearch, FaUser, FaShoppingCart } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoIosArrowDown } from "react-icons/io";
 
 const Navbar = () => {
+    const [visible, setVisible] = useState(false);
     return (
         <>
             <div className="flex items-center justify-between py-5 font-medium">
                 <img src={Logo} className="w-36" alt="Logo" />
-                <ul className="hidden sm:flex gap-5 gap-5 text-lg text-gray-700">
+                <ul className="hidden sm:flex gap-5 text-lg text-gray-700">
                     <NavLink to="/" className="flex flex-col items-center gap-1">
                         <p>Home</p>
                         <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
@@ -47,11 +50,23 @@ const Navbar = () => {
                     </div>
 
                     <Link to="/cart" className='relative'>
-                    <button className='w-5 min-w-5 cursor-pointer'><FaShoppingCart className="text-xl"/></button>
-                    <p className='absolute right-[-7px] bottom-[-2px] w-4 text-center leading-4 bg-white text-black aspect-square border border-black rounded-full text-[10px] '>10</p>
+                        <button className='w-5 min-w-5 cursor-pointer'><FaShoppingCart className="text-xl" /></button>
+                        <p className='absolute -right-1.75 -bottom-0.5 w-4 text-center leading-4 bg-white text-black aspect-square border border-black rounded-full text-[10px] '>10</p>
                     </Link>
+                    <button onClick={() => setVisible(true)} className='w-5 cursor-pointer sm:hidden'><RxHamburgerMenu className='text-2xl' /></button>
                 </div>
-
+                <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? "w-full" : "w-0"}`}>
+                    <div className="flex flex-col text-gray-600">
+                        <div onClick={() => setVisible(false)} className="flex items-center gap-1 p-3 cursor-pointer">
+                            <button className='h-4 rotate-90'><IoIosArrowDown /></button>
+                            <p>Back</p>
+                        </div>
+                        <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border" to="/">Home</NavLink>
+                        <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border" to="/collection">Collection</NavLink>
+                        <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border" to="/about">About</NavLink>
+                        <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border" to="/contact">Contact</NavLink>
+                    </div>
+                </div>
             </div>
         </>
     )
