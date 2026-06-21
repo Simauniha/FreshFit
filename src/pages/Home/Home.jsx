b29730ef12e65b8834202de6bc26e15465762112
Simauniha <2005.simaunihao@gmail.com>
2026-06-21 15:31:31 +0530

    home image updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import "./Home.css";
 import Hero from "../../components/Hero"
 import LatestCollection from '../../components/LatestCollection';
 import BestSheller from '../../components/BestSeller';
