60e0b4937115d48886cb3935d8e30d6d003914ed
Simauniha <2005.simaunihao@gmail.com>
2026-06-29 02:40:47 +0530

    cart page in process

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,10 +1,28 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../../Context/ShopContext'
 
 const Cart = () => {
 
   const { currency, products, cartItems } = useContext(ShopContext);
   const [cartData, setCartData] = useState([])
+
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+tempData.push({
+  _id:items,
+  size:item,
+  quantity:cartItems[items][item]
+})
+        }
+      }
+    }
+    // console.log(tempData);
+    setCartData(tempData);
+    
+  }, [cartItems])
 
   return (
     <></>
