f12d729838beb71d828bf3c7c2ade3bd0a165de7
Simauniha <2005.simaunihao@gmail.com>
2026-07-13 14:47:36 +0530

    orders page updated

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -26,6 +26,13 @@ const Orders = () => {
                   <p className='mt-2'>Date: <span className="text-gray-400">14 JUL 2026</span></p>
                 </div>
               </div>
+              <div className="flex justify-between">
+                <div className="flex items-center gap-2">
+                  <p className="min-w-2 h-2 rounded-full bg-green-600"></p>
+                  <p className="text-sm md:text-base">Ready to Ship</p>
+                </div>
+              </div>
+              <button className="border px-4 py-2 text-sm font-medium">Track Your Order</button>
             </div>
           ))
         }
