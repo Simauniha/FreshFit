6957d39623144d86e4e0aca14133aa47a76b91ba
Simauniha <2005.simaunihao@gmail.com>
2026-07-16 08:30:18 +0530

    login page started

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Title from "../../components/Title"
 
 const Login = () => {
+  const [currentState, setCurrentState] = useState('Login');
   return (
     <>
+      <form action="" className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-18 gap-4 text-gray-800'>
+        <div className="inline-flex items-center gap-2 mb-2 mt-10">
+          <p className="prata-regular text-5xl">
+            {currentState}
+          </p>
+          <hr className="border-none h-[1.5px] w-8 bg-(--primary-900)" />
+        </div>
+      </form>
+
     </>
   )
 }
